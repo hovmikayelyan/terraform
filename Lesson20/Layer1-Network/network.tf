d1a1d70e87620bb3b64cef5ceaeb1b23129c76b2
Hovhannes Mikayelyan <su-hmikayelyan@technamin.com>
2023-03-14 01:58:06 +0400

    Add Lesson20

---
+++ Lesson20/Layer1-Network/network.tf
@@ -0,0 +1,70 @@
+#-------------------------------------------------
+# My Terraform
+#
+# save .tfstate in s3
+#
+# Made by Hov Mikayelyan
+#-------------------------------------------------
+
+provider "aws" {
+  region = "eu-west-3"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "hovs-terraform-project"        // Bucket where to SAVE Terraform State
+    key    = "dev/network/terraform.tfstate" // Object name in the bucket to SAVE Terraform State
+    region = "eu-west-3"                     // Region where bycket created
+  }
+}
+
+#----------------------------------------------------------
+
+data "aws_availability_zones" "available" {}
+
+#----------------------------------------------------------
+
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+  tags = {
+    Name = "${var.owner}'s VPC"
+    Env  = var.env
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "${var.env}-igw"
+  }
+}
+
+
+resource "aws_subnet" "public_subnets" {
+  count                   = length(var.public_subnet_cidrs)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = element(var.public_subnet_cidrs, count.index) # OR -> var.public_subnet_cidrs[count.index]
+  # difference between element and taking by [index], is that if its an object, like map, you should use 'element()' cause maybe you don't know the index !!
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
+  tags = {
+    "Name" = "${var.env}-public-${count.index + 1}"
+  }
+}
+
+resource "aws_route_table" "public_subnets" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+  tags = {
+    Name = "${var.env}-route-public-subnets"
+  }
+}
+
+resource "aws_route_table_association" "public_subnets" {
+  count          = length(aws_route_table.public_subnets[*].id)
+  route_table_id = aws_route_table.public_subnets.id
+  subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
+}
